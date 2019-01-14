02ccf7bb57976222d2168ca2ab9731cea9b49786
davidangulocorcuera <davidangulocorcuera@gmail.com>
2019-01-14 12:12:53 +0100

    cleaning code

--- app/src/main/java/com/example/biolizard/pushexample/MainActivity.kt
+++ app/src/main/java/com/example/biolizard/pushexample/MainActivity.kt
@@ -6,22 +6,17 @@ import android.content.Context
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v4.app.FragmentActivity
-import android.util.Log
-import com.google.firebase.messaging.RemoteMessage
 
 
 
 class MainActivity : AppCompatActivity() {
-    val TAG = "Firebase"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         createNotificationChannel()
     }
+    // Crear el canal para android 8
     private fun createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val name = getString(R.string.channel_name)
             val descriptionText = getString(R.string.channel_description)
@@ -29,7 +24,6 @@ class MainActivity : AppCompatActivity() {
             val channel = NotificationChannel(MyFirebaseMessagingService.CHANNEL_ID, name, importance).apply {
                 description = descriptionText
             }
-            // Register the channel with the system
             val notificationManager: NotificationManager =
                 getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(channel)
--- app/src/main/java/com/example/biolizard/pushexample/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/biolizard/pushexample/MyFirebaseMessagingService.kt
@@ -1,12 +1,9 @@
 package com.example.biolizard.pushexample
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
+
 import android.app.PendingIntent
 import android.app.TaskStackBuilder
-import android.content.Context
 import android.content.Intent
-import android.os.Build
 import android.support.v4.app.NotificationCompat
 import android.support.v4.app.NotificationManagerCompat
 import android.util.Log
@@ -15,18 +12,16 @@ import com.google.firebase.messaging.FirebaseMessagingService
 
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
+    companion object {
+        val CHANNEL_ID = "1"
+        val NOTIFICATION_ID = 1
+    }
+
+    override fun onNewToken(token: String?) {
+        Log.v("token" , token)
+    }
+
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        Log.d(TAG, "From: " + remoteMessage!!.from!!)
-        Log.d(TAG, "Notification Message Body: " + remoteMessage.notification!!.body!!)
-        // ...
-
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
-        Log.d(TAG, "From: " + remoteMessage.from!!)
-
-
-
-        // Check if message contains a notification payload.
         if (remoteMessage.notification != null) {
 
             remoteMessage.notification?.body?.let {
@@ -36,22 +31,13 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         }
     }
 
-    companion object {
-        private val TAG = "FCM Service"
-        val CHANNEL_ID = "1"
-        val NOTIFICATION_ID = 1
-    }
-    fun createNotification(message: String){
-
+    fun createNotification(message: String) {
         val resultIntent = Intent(this, MainActivity::class.java)
         val resultPendingIntent: PendingIntent? = TaskStackBuilder.create(this).run {
             addNextIntentWithParentStack(resultIntent)
             getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
         }
-
-
-
-        var mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+         var mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
             .setSmallIcon(R.drawable.notification_icon_background)
             .setContentTitle("hola")
             .setContentText(message)
@@ -60,11 +46,9 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
             .setAutoCancel(true)
 
         with(NotificationManagerCompat.from(this)) {
-            // notificationId is a unique int for each notification that you must define
             notify(NOTIFICATION_ID, mBuilder.build())
         }
     }
-
 
 
 }
